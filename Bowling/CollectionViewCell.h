62a15453802692f69eb4b6742dc84cc9228e9883
Larry Feldman <larry.feldman@maurice.net>
2016-10-24 18:12:59 -0500

    Lots of changes.

---
+++ Bowling/CollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.h
+//  Bowling
+//
+//  Created by Maurice on 10/24/16.
+//  Copyright © 2016 Larry Feldman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewCell : UICollectionViewCell
+
+@end
